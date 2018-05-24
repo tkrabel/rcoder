5a6b0615c7e31fb313bbb8a6e9f1775f0d1891ae
tkrabel <tobiaskrabel@gmx.de>
2018-05-24 23:36:06 +0200

    ADD clone repo

--- R/clone_r_repos.R
+++ R/clone_r_repos.R
@@ -26,8 +26,40 @@ first_repo <- r_repos[1]
 
 # Clone repo
 project_root <- getwd()
+clone_dir <- "data/repos"
+if (!dir.exists(clone_dir)) dir.create(clone_dir)
+setwd(clone_dir)
 
-if (!dir.exists("~/Desktop/git")) dir.create("~/Desktop/git")
-setwd("~/Desktop/git")
-system(glue("git clone https://github.com{first_repo}.git"))
+for (repo in r_repos) {
+  cat("- Cloning repo", repo, "-\n")
+  folder <- str_replace(repo, "/[^/]+/", "")
+  if (!dir.exists(folder)) {
+    system(glue("git clone https://github.com{repo}.git"),
+           wait = TRUE)
+    # Delete unnecessary files
+    all_files <- list.files(path = folder, recursive = TRUE, full.names = TRUE, 
+                            all.files = TRUE)
+    r_files <- list.files(path = folder, recursive = TRUE, all.files = TRUE,
+                         full.names = TRUE, pat = "\\.R$")
+    unlink(setdiff(all_files, r_files))
+    
+    # Delete empty folders
+    non_empty_dirs <- 
+      list.files(path = folder, recursive = TRUE, full.names = TRUE, 
+                 all.files = TRUE) %>%
+      dirname() %>%
+      unique()
+    all_dirs <- list.dirs(folder) %>% setdiff(folder)
+    empty_dirs <- all_dirs %>% setdiff(non_empty_dirs)
+    unlink(empty_dirs, recursive = TRUE)
+    
+    # Wait
+    secs <- sample(1:10, 1)
+    cat("Sleep for", secs, "seconds ... -.- zZz \n")
+    Sys.sleep(secs)
+  } else {
+    cat("Repo already cloned\n")
+  } # !dir.exists(folder)
+}
+
 setwd(project_root)
