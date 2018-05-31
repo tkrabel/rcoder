f10d8e66767802eee7f6797b633c2b12dc6e4431
tkrabel <tobiaskrabel@gmx.de>
2018-05-31 14:13:30 +0200

    ADD save to txt

--- R/concatenate_r_files.R
+++ R/concatenate_r_files.R
@@ -6,8 +6,12 @@ library(purrr)
 library(magrittr)
 library(readr)
 
-
 r_files <- list.files(path = "data/repos", pat = "\\.r", ignore.case = TRUE,
                       recursive = TRUE, full.names = TRUE)
 text_train <- map_chr(r_files, readr::read_file) %>% paste(collapse = "")
+
+# R data format
 saveRDS(text_train, "data/r_scripts_text.rds")
+
+# Plain text format
+write(text_train, "data/r_scripts_text.txt")
