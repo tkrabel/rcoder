39a6c410b4cd5f055395ed2fde915a4b24174ec9
tkrabel <tobiaskrabel@gmx.de>
2018-08-03 14:00:46 +0200

    remove unnecessary lines

--- devstuff.R
+++ devstuff.R
@@ -15,13 +15,8 @@ library(rvest)
 source("R/utils/github_api_handlers.R")
 
 # Set up git repo
-# pat <- read_file("~/.ssh/github_pat") %>% gsub("\\n", "", .)
-# use_git(message = "Initial commit :sunglasses:")
-# usethis::use_github(credentials = git2r::cred_ssh_key(),
-#                     auth_token = pat)
-# usethis::use_readme_md()
-
-# Use possibly to improve HHTP request handling
-pos_log <- possibly(mean, otherwise = "Error")
-pos_log(10)
-pos_log("a")
+pat <- read_file("~/.ssh/github_pat") %>% gsub("\\n", "", .)
+use_git(message = "Initial commit :sunglasses:")
+usethis::use_github(credentials = git2r::cred_ssh_key(),
+                    auth_token = pat)
+usethis::use_readme_md()
