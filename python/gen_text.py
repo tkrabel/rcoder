cd1fce2093de29a3d3efaf578372aa1addfeca30
tkrabel <tobiaskrabel@gmx.de>
2018-08-03 13:54:11 +0200

    add files for generating own text

---
+++ python/gen_text.py
@@ -0,0 +1,16 @@
+from textgenrnn import textgenrnn
+
+textgen = textgenrnn(weights_path='rcoder_weights.hdf5',
+                       vocab_path='rcoder_vocab.json',
+                       config_path='rcoder_config.json')
+
+temperatures = [5, 10, 20]
+files = [f'rcoder_text_temp_{t}.txt' for t in temperatures]
+
+for i in range(len(temperatures)):
+    file = files[i]
+    temp = temperatures[i] / 10
+    print("Write to text file", file)
+    textgen.generate_to_file(file,
+                             max_gen_length=30000,
+                             temperature=temp)
