14765e2152af0775c3a0b52cb8c38ed1ee81e580
tkrabel <tobiaskrabel@gmx.de>
2018-08-10 16:23:16 +0200

    parallelize runs

--- python/gen_text.py
+++ python/gen_text.py
@@ -1,4 +1,5 @@
 from textgenrnn import textgenrnn
+from joblib import delayed, Parallel
 
 textgen = textgenrnn(weights_path='rcoder_weights.hdf5',
                        vocab_path='rcoder_vocab.json',
@@ -7,10 +8,17 @@ textgen = textgenrnn(weights_path='rcoder_weights.hdf5',
 temperatures = [5, 8, 10, 12, 15, 20]
 files = [f'rcoder_text_temp_{t}.txt' for t in temperatures]
 
-for i in range(len(temperatures)):
-    file = files[i]
-    temp = temperatures[i] / 10
+def generate_text(file, temperature):
+    temp = temperature / 10
     print("Write to text file", file)
     textgen.generate_to_file(file,
                              max_gen_length=50000,
                              temperature=temp)
+    return None
+
+Parallel(n_jobs=6)(delayed(generate_text)(file=files[i], temperature=temperatures[i]) for i in range(len(temperatures)) )
+
+# for i in range(len(temperatures)):
+#     file = files[i]
+#     temp = temperatures[i] / 10
+    
