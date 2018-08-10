1b3523d65f171bc50fe79c6c2c599a7b55c5f937
tkrabel <tobiaskrabel@gmx.de>
2018-08-10 15:40:17 +0200

    add more temperatures

--- python/gen_text.py
+++ python/gen_text.py
@@ -4,7 +4,7 @@ textgen = textgenrnn(weights_path='rcoder_weights.hdf5',
                        vocab_path='rcoder_vocab.json',
                        config_path='rcoder_config.json')
 
-temperatures = [5, 10, 20]
+temperatures = [5, 8, 10, 12, 15, 20]
 files = [f'rcoder_text_temp_{t}.txt' for t in temperatures]
 
 for i in range(len(temperatures)):
@@ -12,5 +12,5 @@ for i in range(len(temperatures)):
     temp = temperatures[i] / 10
     print("Write to text file", file)
     textgen.generate_to_file(file,
-                             max_gen_length=30000,
+                             max_gen_length=50000,
                              temperature=temp)
